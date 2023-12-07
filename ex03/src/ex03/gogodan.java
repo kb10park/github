d2b0d2cd90cda9cc65a2176de33f094d1482da4b
park kyoung bin <bgbin@naver.com>
2023-12-07 20:15:36 +0900

    4번째 커밋

---
+++ ex03/src/ex03/gogodan.java
@@ -0,0 +1,15 @@
+package ex03;
+
+public class gogodan {
+
+	public static void main(String[] args) {
+	      int i,j;
+	      for(i=3;i<8;i++) {
+	    	  for(j=1;j<10;j++)
+	    		  System.out.printf("%d*%d=%d",i,j,i*j);
+	      }
+	      System.out.println();
+
+	}
+
+}
