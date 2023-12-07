286c16da75269d6da33f9b5b6eaa1dc3915d7fc3
park kyoung bin <bgbin@naver.com>
2023-12-07 20:18:16 +0900

    8번째 커밋

---
+++ ex03/src/ex03/Profile.java
@@ -0,0 +1,18 @@
+package ex03;
+public class Profile {
+
+	public static void main(String[] args) {
+		String name="홍길동";
+		int studentnumber=20202694;
+		double tall=1.78;
+		boolean isMale= true;
+		
+		System.out.println("이름:"+name);
+		System.out.println("학번:"+studentnumber);
+		System.out.println("키:"+tall);
+		System.out.println("남자냐:"+isMale);
+		
+
+	}
+
+}
