fbf3791518ca63204c82ffef4167db4a390d501c
park kyoung bin <bgbin@naver.com>
2023-12-07 20:37:43 +0900

    bar 1번째

---
+++ ex03/src/ex03/User.java
@@ -0,0 +1,56 @@
+package ex03;
+import java.util.ArrayList;
+public class User {
+	private static String id;
+	private static String pw;
+	private String name;
+	private int age;
+	private String address;
+	private String gender;
+	
+	private static ArrayList<ArrayList<String>>arrlist=new ArrayList<>();
+	
+	public static ArrayList<ArrayList<String>>getArrList(){
+		return arrlist;
+	}
+	public static void setArrList(ArrayList<ArrayList<String>>arrList) {
+		User.arrlist=arrList;
+	}
+	public static String getId() {
+		return id;
+	}
+	public static void setId(String id) {
+		User.id=id;
+	}
+	public static String getPw() {
+		return pw;
+	}
+	public static void setPw(String pw) {
+		User.pw=pw;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age=age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address=address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender=gender;
+	}
+
+}
