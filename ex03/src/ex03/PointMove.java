aa992d2d40f0865578d27c59de91019d450473e8
park kyoung bin <bgbin@naver.com>
2023-12-07 20:36:09 +0900

    foo 4번째 커밋

---
+++ ex03/src/ex03/PointMove.java
@@ -0,0 +1,10 @@
+package ex03;
+
+public class PointMove {
+
+	public static void main(String[] args) {
+		Point point1 = new Point();  // 객체 생성
+		point1.move(10,10);            // 메시지
+		System.out.print("x = " + point1.xPosition + " y 			= " + point1.yPosition);
+	}
+}
